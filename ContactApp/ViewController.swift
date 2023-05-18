12caba5287648ca4fdc3dd285c0d58a332f098da
ajihairi <hamzhyatinnov@gmail.com>
2023-05-18 19:03:00 +0700

    Initial Commit

---
+++ ContactApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactApp
+//
+//  Created by Hamzhya Salsatinnov on 18/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
